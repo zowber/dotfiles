8e121fe167d20421edd76f0d71bc60d2202743c4
Andy Bright <andy@zowber.com>
2017-11-05 13:03:45 +0000

    ranger stubs

---
+++ ranger/colorschemes/1984.py
@@ -0,0 +1,22 @@
+#Based on https://www.reddit.com/r/unixporn/comments/69008j/i3gaps_1984/
+
+from __future__ import (absolute_import, division, print_function)
+
+from ranger.colorschemes.default import Default
+from ranger.gui.color import yellow, red, blue
+
+
+class Scheme(Default):
+    progress_bar_color = red
+
+    def use(self, context):
+        fg, bg, attr = Default.use(self, context)
+
+        if context.directory and not context.marked and not context.link \
+                and not context.inactive_pane:
+            fg = red
+
+        if context.in_titlebar and context.hostname:
+            fg = yellow if context.bad else blue
+
+        return fg, bg, attr
