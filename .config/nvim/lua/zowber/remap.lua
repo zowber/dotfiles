65257ad34c49db5d47960bb85148c65f232a1a06
zowber <andy@zowber.com>
2023-11-09 17:07:42 +0000

    fix: missing closing paren

--- .config/nvim/lua/zowber/remap.lua
+++ .config/nvim/lua/zowber/remap.lua
@@ -4,4 +4,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv"
+vim.keymap.set("n", "N", "Nzzzv")
